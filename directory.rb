2aeca8101ca368c50e552d2f63416b093ce2055a
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-24 13:45:22 +0100

    Stage 8 - Exercise 1

--- directory.rb
+++ directory.rb
@@ -1,5 +1,3 @@
-# let's put all students into an array
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -19,28 +17,14 @@ def input_students
   students
 end
 
-# students = [
-#   {name: "Dr. Hannibal Lecter", cohort: :november},
-#   {name: "Darth Vader", cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "Alex Delarge", cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Krueger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffery Baratheon", cohort: :november},
-#   {name: "Norman Bates", cohort: :november}
-# ]
-
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each.with_index(1) do |student,index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
