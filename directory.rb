5a3644c8abc910bc5f4b108968c57120907ef507
Rob Adams <robert.andrew.adams@gmail.com>
2016-10-25 20:05:36 +0100

    Stage 10 - Refactoring code Also corrected a couple of spelling mistakes

--- directory.rb
+++ directory.rb
@@ -1,30 +1,42 @@
 FORMAT_WIDTH = 54 # A Constant (CONST) value
+@students = []
 
 def interactive_menu
-  system "clear"
-  @students = []
+  # system "clear"
   loop do
-    # system "clear"
-    # print menu
-    puts """\n
-    \n1. Input the students
-    \n2. Show the students
-    \n9. Exit"""
-    # get user input
-    selection = gets.chomp
-    # do action
-    case selection
-      when "1"
-        @students = input_students
-      when "2"
-        print_header
-        print_data(@students,1,"")
-        print_footer(@students,"")
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)# do action
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+# get user input
+def print_menu
+  puts """
+  1. Input the students
+  2. Show the students
+  9. Exit"""
+end
+
+def show_students
+  if @students.empty?
+    puts "You have no students yet :-("
+  else
+    print_header
+    print_data(@students,"")
+    print_footer(@students,"")
   end
 end
 
@@ -53,31 +65,31 @@ def input_students
     puts "Name: #{name}\nCohort: #{cohort}\nAge: #{age}\nGender: #{sex}\nFavourite hobby: #{hobby}"
     print "Y/N?"
     check = gets.chomp
+
     if check == "Y"
       @students << {name: name, cohort: cohort.to_sym, age: age, sex: sex, hobby: hobby}
       puts "Now we have #{@students.count} student#{@students.count == 1 ? "": "s"}"
     end
-    if @students.empty?
-      #####
-    end
+
+
   end
   # return the array of students
   @students
 end
 
 def print_header
-  system "clear"
+  # system "clear"
   puts "The students of Villains Academy".center(FORMAT_WIDTH)
   puts "-------------".center(FORMAT_WIDTH)
 end
 
-def print_data(array,offset,filter)
+def print_data(array, filter)
   puts "--------------------------------".center(FORMAT_WIDTH)
   i = 0
   until i >= array.length
-    array.each.with_index(offset) do |student,index| # requires the each removing
+    array.each.with_index(1) do |student,index| # requires the each removing
       if filter == ""
-          puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort), they are #{:age} years old and thier favourite hobby is #{:hobby})"
+          puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort), they are #{student[:age]} years old and their favourite hobby is #{student[:hobby]}"
       elsif filter =~ /^[A-z]+$/ # true if only word
         @filter_type = "whose names begin with"
         if student[:name] =~ /^#{filter}/i
@@ -96,21 +108,21 @@ def print_data(array,offset,filter)
 end
 
 def print_footer(array,filter)
-  puts "Overall, we have #{array.count} great students#{filter} ".center(FORMAT_WIDTH)
+  puts "Overall, we have #{array.count} great student#{@students.count == 1 ? "": "s"}#{filter} ".center(FORMAT_WIDTH)
 end
 
 def print_filtered_footer(array)
   puts "These are students #{@filter_type} #{@filter}".center(FORMAT_WIDTH)
 end
 
-def print_by_sort(array,sort_key,offset,filter)
+def print_by_sort(array, sort_key, filter)
   array_by_sort = array.sort_by {|v| v[sort_key]}
-  print_data(array_by_sort,offset,filter)
+  print_data(array_by_sort,filter)
 end
 
-def print_by_sort_and_filter(array,sort_key,filter_key,offset,filter)
+def print_by_sort_and_filter(array, sort_key, filter_key, filter)
   array_by_sort_and_filter = array.select { |hash| hash[sort_key.to_sym] == filter_key.to_sym}.sort_by {|v| v[sort_key.to_sym]}.sort_by {|v| v[:name]}
-  print_data(array_by_sort_and_filter,offset,filter)
+  print_data(array_by_sort_and_filter, filter)
 end
 # nothing happens until we call the methods:
 # students = input_students
